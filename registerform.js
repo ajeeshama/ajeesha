1f30241501231b0f1d6dce92df127342c08cd8e9
ajeeshama <45811881+ajeeshama@users.noreply.github.com>
2018-12-14 12:13:41 +0530

    Create registerform.js

---
+++ registerform.js
@@ -0,0 +1,31 @@
+function valudate form(){
+x=document get element BYId("email")value
+if(x==""){alert("email must be failed");
+return false;
+}
+function valudate form(){
+x=document get element BYId("password")value
+if(x=="){alert("password must be failed");
+return false;
+}
+x=document get element BYId("username")value
+if(x==""){alert("username must be failed");
+return false;
+}
+x=document get element BYId("degree")value
+if(x==""){alert("degree must be failed");
+return false;
+}
+x=document get element BYId("branch")value
+if(x==""){alert("branch must be failed");
+return false;
+}
+x=document get element BYId("repeat password")value
+if(x==""){alert("repeat password must be failed");
+return false;
+}
+x=document get element BYId("college name")value
+if(x==""){alert("college name must be failed");
+return false;
+}
+}
