3adee80d7d45ff650915ff57931b05cbdd4df6fd
ajeeshama <45811881+ajeeshama@users.noreply.github.com>
2018-12-14 12:15:16 +0530

    Create loginpage.js

---
+++ loginpage.js
@@ -0,0 +1,10 @@
+function valudate form(){
+x=document get element BYId("email")value
+if(x==""){alert("email must be failed");
+return false;
+}
+function valudate form(){
+x=document get element BYId("password")value
+if(x=="){alert("password must be failed");
+return false;
+}
